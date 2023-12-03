70854e2fd8397a1127c276f5cf3d87819905a86e
zynpekzlrt <zynpekzlr@gmail.com>
2023-12-04 00:53:20 +0300

    first commit

---
+++ state/src/App.js
@@ -0,0 +1,31 @@
+import Course from './Course';
+import './App.css';
+import {useState} from 'react';
+
+
+function getRandomCourse(){
+  const courseArray = ['Angular','Bootstrap','Ccsharp','KompleWeb'];
+  return courseArray[Math.floor(Math.random() * courseArray.length)];
+}
+
+function App() {
+  const [courses, setCourses] = useState([]);
+
+  const handleClick = () => {
+    setCourses([...courses,getRandomCourse()]);
+  };
+  const courseList = courses.map((course,index)=>{
+    return <Course key={index} courseName={course} />;
+   });
+  return (
+    <div className="App">
+
+      <button className='appButton' onClick={handleClick}>Kurs Ekle</button> 
+      <div className='courseList'>
+        {courseList}
+      </div>
+    </div>
+  );
+}
+
+export default App;
